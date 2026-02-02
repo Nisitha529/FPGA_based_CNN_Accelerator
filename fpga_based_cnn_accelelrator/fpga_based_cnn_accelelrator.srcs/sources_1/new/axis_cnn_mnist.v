81f43ab553e3d2d01721da08985ac046d25fe0b8
Nisitha529 <silvanisithamaneesha@gmail.com>
2026-02-02 23:56:43 +0530

    creating_src_files

---
+++ fpga_based_cnn_accelelrator/fpga_based_cnn_accelelrator.srcs/sources_1/new/axis_cnn_mnist.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 02/02/2026 11:50:19 PM
+// Design Name: 
+// Module Name: axis_cnn_mnist
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module axis_cnn_mnist(
+
+    );
+endmodule
